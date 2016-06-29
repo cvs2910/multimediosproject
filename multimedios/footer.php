becaedb0c9374e8563dae0e6298fcee654e895cd
cvs2910 <cvs2910@gmail.com>
2016-06-29 14:56:41 -0600

    Add files via upload

--- multimedios/footer.php
+++ multimedios/footer.php
@@ -1,5 +1,3 @@
-
-
 <footer>
 	
 <div id="divider">
@@ -12,11 +10,15 @@
 
           <!-- Footer Starts -->
 <div class="footer text-center spacer">
-<p class="wowload flipInX"><a href="#"><i class="fa fa-facebook fa-2x"></i></a> <a href="#"><i class="fa fa-instagram fa-2x"></i></a> <a href="#"><i class="fa fa-twitter fa-2x"></i></a> <a href="#"><i class="fa fa-flickr fa-2x"></i></a> </p>
+<p class="wowload flipInX"><a href="#" onclick="ga('send','event','Index','click','btnFaceBook')"><i class="fa fa-facebook fa-2x"></i></a> 
+<a href="#" onclick="ga('send','event','Index','click','btnInstagram')"><i class="fa fa-instagram fa-2x"></i></a>
+ <a href="#" onclick="ga('send','event','Index','click','btnTwitter')"><i class="fa fa-twitter fa-2x"></i></a>
+ <a href="#" onclick="ga('send','event','Index','click','btnFlickr')"><i class="fa fa-flickr fa-2x"></i></a>
+ </p>
 © 2016 Empresarios Unidos De Puntarenas S.A. - Todos los derechos reservados
 </div>
 <!-- # Footer Ends -->
-<a href="#home" class="gototop "><i class="fa fa-angle-up  fa-3x"></i></a>
+<a href="#home" class="gototop " onclick="ga('send','event','Index','click','btnGoToTop')"><i class="fa fa-angle-up  fa-3x"></i></a>
 
 
 
